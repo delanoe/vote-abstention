e4664b6068d16f4d7a4ebe50701911d0d5dc5174
Alexandre Delanoë <anoe@debian.org>
2017-04-30 21:06:18 +0200

    Vote estimation according to Galam Paper on Arxiv : https://arxiv.org/abs/1703.04643

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,24 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+
+-- | Intention of Vote
+type Intention  = Double
+
+-- | Estimated abstention
+type Abstention = Double
+
+vote :: Intention -> Abstention -> (Abstention, Abstention) -> (Double, Double)
+vote i a (a1, a2) = (a' / total, b' / total)
+    where
+        ia = i
+        ib = 1 - i
+
+        a' = ia * a1 * a
+        b' = ib * a2 * a
+        
+        total = a' + b'
+
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
