0a5ef63b9c54e883e63a55dc4450487f076793cf
Alexandre Delanoë <anoe@debian.org>
2017-04-30 21:07:09 +0200

    Global Abstention not effective.

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,14 +7,14 @@ type Intention  = Double
 -- | Estimated abstention
 type Abstention = Double
 
-vote :: Intention -> Abstention -> (Abstention, Abstention) -> (Double, Double)
-vote i a (a1, a2) = (a' / total, b' / total)
+vote :: Intention -> (Abstention, Abstention) -> (Double, Double)
+vote i (a1, a2) = (a' / total, b' / total)
     where
         ia = i
         ib = 1 - i
 
-        a' = ia * a1 * a
-        b' = ib * a2 * a
+        a' = ia * a1
+        b' = ib * a2
         
         total = a' + b'
 
